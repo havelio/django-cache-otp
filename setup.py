5898f58b186d99ed088a853b41eed40aaca49096
havelio <havelioh@gmail.com>
2020-04-25 10:42:35 +0700

    add setup packages

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
-    name='django_cache_otp',
+    name='django_otp_cache',
     version='v0.1',
-    packages=['django_cache_otp'],
+    packages=['django_otp_cache'],
     description='one time password based on django cache',
     long_description=README,
     author='havelio',
     author_email='havelioh@gmail.com',
-    url='https://github.com/havelio/django-cache-otp/',
+    url='https://github.com/havelio/django-otp-cache/',
     license='MIT',
     install_requires=[
         'Django>=2',
